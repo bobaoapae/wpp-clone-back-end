54db8a8e66d117e104e478a65138e0db28dc4de5
João Vitor Borges <33332428+bobaoapae@users.noreply.github.com>
2019-12-05 14:32:29 -0300

    Update lib wpp, removido cache dos json

--- src/main/java/br/com/zapia/catarin/whatsApp/SerializadorWhatsApp.java
+++ src/main/java/br/com/zapia/catarin/whatsApp/SerializadorWhatsApp.java
@@ -4,9 +4,6 @@ import br.com.zapia.catarin.utils.Util;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
 import modelo.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -16,22 +13,18 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Scope("usuario")
 @Service
 public class SerializadorWhatsApp {
 
     private ObjectMapper objectMapper;
-    private LoadingCache<WhatsappObjectWithId, ObjectNode> cache;
-    private LoadingCache<Chat, ObjectNode> cacheChats;
-    private LoadingCache<Message, ObjectNode> cacheMsgs;
+    private Logger log = Logger.getLogger(SerializadorWhatsApp.class.getName());
     @Lazy
     @Autowired
     private SerializadorWhatsApp serializadorWhatsApp;
@@ -39,76 +32,16 @@ public class SerializadorWhatsApp {
     @PostConstruct
     public void init() {
         this.objectMapper = new ObjectMapper();
-        cache = CacheBuilder.newBuilder()
-                .maximumSize(1000)
-                .concurrencyLevel(Runtime.getRuntime().availableProcessors() * 2)
-                .expireAfterWrite(1, TimeUnit.DAYS)
-                .build(
-                        new CacheLoader<>() {
-                            public ObjectNode load(WhatsappObjectWithId whatsappObject) throws IOException {
-                                return (ObjectNode) objectMapper.readTree(whatsappObject.toJson());
-                            }
-                        });
-        cacheChats = CacheBuilder.newBuilder()
-                .maximumSize(200)
-                .concurrencyLevel(Runtime.getRuntime().availableProcessors() * 2)
-                .expireAfterWrite(1, TimeUnit.HOURS)
-                .build(
-                        new CacheLoader<>() {
-                            public ObjectNode load(Chat chat) throws ExecutionException {
-                                ObjectNode chatNode;
-                                try {
-                                    chatNode = cache.get(chat);
-                                } catch (IllegalStateException e) {
-                                    cache.invalidate(chat);
-                                    chatNode = cache.get(chat);
-                                }
-                                chatNode.putObject("contact").setAll(cache.get(chat.getContact()));
-                                chatNode.put("picture", chat.getContact().getThumb());
-                                chatNode.put("type", chat.getJsObject().getProperty("kind").asString().getValue());
-                                ((ObjectNode) chatNode.get("contact")).remove("type");
-                                chatNode.remove(Arrays.asList("lastReceivedKey", "pendingMsgs"));
-                                return chatNode;
-                            }
-                        });
-        cacheMsgs = CacheBuilder.newBuilder()
-                .maximumSize(2000)
-                .concurrencyLevel(Runtime.getRuntime().availableProcessors() * 2)
-                .expireAfterWrite(1, TimeUnit.HOURS)
-                .build(
-                        new CacheLoader<>() {
-                            public ObjectNode load(Message message) throws ExecutionException {
-                                ObjectNode msgNode;
-                                try {
-                                    msgNode = cache.get(message);
-                                } catch (IllegalStateException e) {
-                                    cache.invalidate(message);
-                                    msgNode = cache.get(message);
-                                }
-                                if (message.getSender() != null && (message.getChat() instanceof GroupChat)) {
-                                    msgNode.putObject("sender").setAll(cache.get(message.getSender()));
-                                }
-                                if (message instanceof MediaMessage) {
-                                    msgNode.put("filename", ((MediaMessage) message).getFileName());
-                                    if (message.getJsObject().hasProperty("pageCount") && message.getJsObject().getProperty("pageCount").isNumber()) {
-                                        msgNode.put("pageCount", message.getJsObject().getProperty("pageCount").asNumber().getValue());
-                                    }
-                                }
-                                msgNode.remove(Arrays.asList("self", "invis", "clientUrl", "directPath", "filehash", "uploadhash", "mediaKey", "mediaKeyTimestamp"));
-                                return msgNode;
-                            }
-                        });
     }
 
     @Async
     public CompletableFuture<ObjectNode> serializarChat(Chat chat) throws ExecutionException {
-        ObjectNode chatNode;
-        try {
-            chatNode = cacheChats.get(chat);
-        } catch (IllegalStateException e) {
-            cacheChats.invalidate(chat);
-            chatNode = cacheChats.get(chat);
-        }
+        ObjectNode chatNode = converterParaObjectNode(chat);
+        Objects.requireNonNull(chatNode).putObject("contact").setAll(Objects.requireNonNull(converterParaObjectNode(chat.getContact())));
+        chatNode.put("picture", chat.getContact().getThumb());
+        chatNode.put("type", chat.getJsObject().getProperty("kind").asString().getValue());
+        ((ObjectNode) chatNode.get("contact")).remove("type");
+        chatNode.remove(Arrays.asList("lastReceivedKey", "pendingMsgs"));
         ArrayNode arrayNode = objectMapper.createArrayNode();
         List<Message> allMessages = chat.getAllMessages();
         int partitionSize = allMessages.size() < Runtime.getRuntime().availableProcessors() ? allMessages.size() : allMessages.size() / Runtime.getRuntime().availableProcessors();
@@ -140,15 +73,18 @@ public class SerializadorWhatsApp {
 
     @Async
     public CompletableFuture<ObjectNode> serializarMsg(Message message) throws ExecutionException {
-        ObjectNode objectNode;
-        try {
-            objectNode = cacheMsgs.get(message);
-        } catch (IllegalStateException e) {
-            cacheMsgs.invalidate(message);
-            objectNode = cacheMsgs.get(message);
+        ObjectNode msgNode = Objects.requireNonNull(converterParaObjectNode(message));
+        if (message.getSender() != null && (message.getChat() instanceof GroupChat)) {
+            msgNode.putObject("sender").setAll(Objects.requireNonNull(converterParaObjectNode(message.getSender())));
         }
-        objectNode.put("ack", (Double) utils.Util.convertJSValue(message.getJsObject().getProperty("ack")));
-        return CompletableFuture.completedFuture(objectNode);
+        if (message instanceof MediaMessage) {
+            msgNode.put("filename", ((MediaMessage) message).getFileName());
+            if (message.getJsObject().hasProperty("pageCount") && message.getJsObject().getProperty("pageCount").isNumber()) {
+                msgNode.put("pageCount", message.getJsObject().getProperty("pageCount").asNumber().getValue());
+            }
+        }
+        msgNode.remove(Arrays.asList("self", "invis", "clientUrl", "directPath", "filehash", "uploadhash", "mediaKey", "mediaKeyTimestamp"));
+        return CompletableFuture.completedFuture(msgNode);
     }
 
     @Async
@@ -162,5 +98,14 @@ public class SerializadorWhatsApp {
             }
         });
         return CompletableFuture.completedFuture(arrayNode);
+    }
+
+    private ObjectNode converterParaObjectNode(WhatsappObject whatsappObject) {
+        try {
+            return (ObjectNode) objectMapper.readTree(whatsappObject.toJson());
+        } catch (IOException e) {
+            log.log(Level.SEVERE, "ConverterParaObjectNode", e);
+            return null;
+        }
     }
 }
