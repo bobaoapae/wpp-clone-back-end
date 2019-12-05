7b4797ae56932d7342fe90a84482fc69e35dde6c
João Vitor Borges <33332428+bobaoapae@users.noreply.github.com>
2019-12-05 15:23:22 -0300

    Add log no scope

--- src/main/java/br/com/zapia/catarin/authentication/scopeInjectionHandler/UsuarioScoped.java
+++ src/main/java/br/com/zapia/catarin/authentication/scopeInjectionHandler/UsuarioScoped.java
@@ -1,35 +1,49 @@
 package br.com.zapia.catarin.authentication.scopeInjectionHandler;
 
+import br.com.zapia.catarin.modelo.Usuario;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.config.Scope;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.logging.Logger;
 
 public class UsuarioScoped implements Scope {
 
+    private static final Logger log = Logger.getLogger(Usuario.class.getName());
+
     private final Map<String, Object> scopes = new ConcurrentHashMap<>();
-    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    private final ReentrantLock readWriteLock = new ReentrantLock();
 
 
     @Override
     public Object get(String s, ObjectFactory<?> objectFactory) {
         try {
-            readWriteLock.writeLock().lock();
+            log.info("UsuarioScoped -> get");
+            readWriteLock.lock();
+            log.info("UsuarioScoped -> lock");
             if (!scopes.containsKey(getConversationId() + s)) {
                 scopes.put(getConversationId() + s, objectFactory.getObject());
             }
         } finally {
-            readWriteLock.writeLock().unlock();
+            readWriteLock.unlock();
+            log.info("UsuarioScoped -> unlock");
         }
         return scopes.get(getConversationId() + s);
     }
 
     @Override
     public Object remove(String s) {
-        return scopes.remove(getConversationId() + s);
+        try {
+            log.info("UsuarioScoped -> remove");
+            readWriteLock.lock();
+            log.info("UsuarioScoped -> lock");
+            return scopes.remove(getConversationId() + s);
+        } finally {
+            readWriteLock.unlock();
+            log.info("UsuarioScoped -> unlock");
+        }
     }
 
     @Override
