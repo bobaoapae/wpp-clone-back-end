181c20ddc495a00b41188be99bcc76e7453e0549
jvbor <jvborges@outlook.com>
2020-02-14 16:51:23 -0300

    Mudança na forma de baixar os arquivos das mensagens do WhatsApp

--- src/main/java/br/com/zapia/wppclone/handlersWebSocket/DownloadMediaHandler.java
+++ src/main/java/br/com/zapia/wppclone/handlersWebSocket/DownloadMediaHandler.java
@@ -1,20 +1,20 @@
 package br.com.zapia.wppclone.handlersWebSocket;
 
 import br.com.zapia.wppclone.modelo.Usuario;
-import br.com.zapia.wppclone.payloads.DownloadMediaResponse;
 import br.com.zapia.wppclone.payloads.WebSocketResponse;
+import br.com.zapia.wppclone.servicos.DownloadMediaService;
 import modelo.MediaMessage;
-import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Base64;
 import java.util.concurrent.CompletableFuture;
-import java.util.logging.Level;
 
 @HandlerWebSocketEvent(event = "downloadMedia")
 public class DownloadMediaHandler extends HandlerWebSocket {
+
+    @Autowired
+    private DownloadMediaService downloadMediaService;
+
     @Override
     public CompletableFuture<WebSocketResponse> handle(Usuario usuario, Object payload) {
         return whatsAppClone.getDriver().getFunctions().getMessageById((String) payload).thenCompose(msg -> {
@@ -29,24 +29,8 @@ public class DownloadMediaHandler extends HandlerWebSocket {
                     }
                 }).thenApply(file -> {
                     if (file != null) {
-                        try {
-                            String contentType = Files.probeContentType(file.toPath());
-                            byte[] data = Files.readAllBytes(file.toPath());
-                            String fileName = ((MediaMessage) msg).getFileName();
-                            if (fileName.isEmpty()) {
-                                fileName = file.getName();
-                            }
-                            String base64str = Base64.getEncoder().encodeToString(data);
-                            StringBuilder sb = new StringBuilder();
-                            sb.append("data:");
-                            sb.append(contentType);
-                            sb.append(";base64,");
-                            sb.append(base64str);
-                            return new WebSocketResponse(HttpStatus.OK, new DownloadMediaResponse(fileName, sb.toString()));
-                        } catch (IOException e) {
-                            whatsAppClone.getLogger().log(Level.SEVERE, "Convert to Base64", e);
-                            return new WebSocketResponse(HttpStatus.INTERNAL_SERVER_ERROR, ExceptionUtils.getMessage(e));
-                        }
+                        String key = downloadMediaService.addFileToFutureDownload(file);
+                        return new WebSocketResponse(HttpStatus.OK, key);
                     } else {
                         return new WebSocketResponse(HttpStatus.INTERNAL_SERVER_ERROR, "file null");
                     }
---
+++ src/main/java/br/com/zapia/wppclone/restControllers/DownloadMediaRestController.java
@@ -0,0 +1,56 @@
+package br.com.zapia.wppclone.restControllers;
+
+import br.com.zapia.wppclone.servicos.DownloadMediaService;
+import org.apache.tika.Tika;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+@RequestMapping("/api/downloadMedia")
+public class DownloadMediaRestController {
+
+    @Autowired
+    private DownloadMediaService downloadMediaService;
+
+    @GetMapping("/{key}")
+    public ResponseEntity<?> downloadMedia(@PathVariable("key") String key) {
+        try {
+            File file = downloadMediaService.getFileToDownload(key);
+            Path path = Paths.get(file.getAbsolutePath());
+            String contentType = new Tika().detect(file);
+            ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getName());
+            headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+            headers.add("Pragma", "no-cache");
+            headers.add("Expires", "0");
+            headers.add("Filename", file.getName());
+            headers.add(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "Filename");
+            downloadMediaService.removeFileToDownload(key);
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .contentLength(file.length())
+                    .contentType(MediaType.parseMediaType(contentType))
+                    .body(resource);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro ao Baixar", e);
+        }
+    }
+
+}
