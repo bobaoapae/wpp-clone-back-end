24c394c41e54f7502c6d487eb39d6fe6715a6a5a
joao borges <jvborges@outlook.com>
2020-04-09 16:40:36 -0300

    Update Lib Wpp

--- src/main/java/br/com/zapia/wppclone/whatsApp/WhatsAppClone.java
+++ src/main/java/br/com/zapia/wppclone/whatsApp/WhatsAppClone.java
@@ -235,7 +235,6 @@ public class WhatsAppClone {
             builder.customScheduledExecutorService(scheduledExecutorServiceSupplier);
             builder.onNeedQrCode(onNeedQrCode);
             builder.headLess(headLess);
-            builder.forceBeta(true);
             builder.addErrorHandler(throwable -> {
                 logger.log(Level.SEVERE, "Error Driver WhatsApp " + usuarioResponsavelInstancia.getLogin(), throwable);
             });
